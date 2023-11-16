098800d4d5945b037ff6012ab14c84a8d94601af
Kamuri Amorim <myk.gata14@gmail.com>
2023-11-16 18:03:41 -0300

    chore: housekeeping

--- variables.oci.tf
+++ variables.oci.tf
@@ -8,9 +8,3 @@ variable "ssh_pub_key" {
   description = "SSH Public Key"
   type        = string
 }
-
-variable "oci_image" {
-  description = "Image for OCI Operating System"
-  type        = string
-  default     = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaalnfzlq4plimtjcnmejd2p3aoecohlj2uxua5vymsribmpiqtq4la"
-}
