f0de2e28bcab1fca1f943dace8b6cf95b9ed5f23
Kamuri Amorim <myk.gata14@gmail.com>
2024-10-25 16:03:23 -0300

    fix: fix ipv6 block

--- network_lb.tf
+++ network_lb.tf
@@ -9,7 +9,7 @@ resource "oci_core_vcn" "lb_vcn" {
   cidr_block     = "11.0.0.0/16"
   dns_label      = "lb"
   is_ipv6enabled = true
-  ipv6private_cidr_blocks = [ "2603:c021:c008:61FF::/64" ]
+  ipv6private_cidr_blocks = [ "2603:c021:c008:61ff::/64" ]
 }
 
 resource "oci_core_subnet" "lb_subnet" {
@@ -19,7 +19,7 @@ resource "oci_core_subnet" "lb_subnet" {
   dns_label      = "lb"
   route_table_id = oci_core_vcn.lb_vcn.default_route_table_id
   vcn_id         = oci_core_vcn.lb_vcn.id
-  ipv6cidr_block = "2603:c021:c008:61FF::/64"
+  ipv6cidr_block = "2603:c021:c008:61ff::/64"
 }
 
 resource "oci_core_drg_attachment" "lb_drg_attachment" {
