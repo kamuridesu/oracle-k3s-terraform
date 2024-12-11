2eaccb73c24b7cf39a1b78c6638c68b2f7e04340
Kamuri Amorim <myk.gata14@gmail.com>
2024-12-11 17:46:23 -0300

    fix: update output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "k3s_public_ip" {
-  description = "Control Plaine Public IP"
+output "k3s_public_ip_ipv4" {
+  description = "Public IPV4 IP for K3s VMs"
   value = {
     for k, v in oci_core_instance.arm64 : k => v.public_ip
   }
